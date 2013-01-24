ad08ca10e20b8e906ed18a15f507e6ccc187dba3
jd <jd@cals.arizona.edu>
2013-01-24 10:49:24 -0700

    added devicePropoerties & deviceConnection
    display device properties and connection to main.html

--- www/js/index.js
+++ www/js/index.js
@@ -34,6 +34,8 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        app.displayDeviceInfo();
+        checkConnection();
         navigator.splashscreen.hide();
     },
     // Update DOM on a Received Event
@@ -49,5 +51,34 @@ var app = {
         navElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    }
+    // display Device info
+    displayDeviceInfo: function() {
+    	var element = document.getElementById('deviceProperties');
+
+        element.innerHTML = 'Device Name: '     + device.name     + '<br />' + 
+                            'Device Cordova: '  + device.cordova  + '<br />' + 
+                            'Device Platform: ' + device.platform + '<br />' + 
+                            'Device UUID: '     + device.uuid     + '<br />' + 
+                            'Device Version: '  + device.version  + '<br />';
+    }
+    
+    //check connection status
+    function checkConnection() {
+        var networkState = navigator.connection.type;
+
+        var states = {};
+        states[Connection.UNKNOWN]  = 'Unknown connection';
+        states[Connection.ETHERNET] = 'Ethernet connection';
+        states[Connection.WIFI]     = 'WiFi connection';
+        states[Connection.CELL_2G]  = 'Cell 2G connection';
+        states[Connection.CELL_3G]  = 'Cell 3G connection';
+        states[Connection.CELL_4G]  = 'Cell 4G connection';
+        states[Connection.NONE]     = 'No network connection';
+
+        //alert('Connection type: ' + states[networkState]);
+        var element = document.getElementById('deviceConnection');
+        element.innerHTML = 'Connection type: ' + states[networkState];
+        
     }
 };
